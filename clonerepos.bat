8ac4482d0e04d6b5caea29b54f28300229ece46e
oliwilliams1 <imhungryoliver@gmail.com>
2025-01-29 20:09:14 +1300

    Add imgui file doalog submodule for real this time

--- clonerepos.bat
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/g-truc/glm dependencies/glm
-git clone https://github.com/Perlmint/glew-cmake dependencies/glew
-git clone https://github.com/glfw/glfw dependencies/glfw
-git clone https://github.com/ocornut/imgui dependencies/imgui
-git clone https://github.com/aiekick/ImGuiFileDialog dependencies/imgui-file-dialog
-git clone https://github.com/nothings/stb dependencies/stb
-git clone https://github.com/mlabbe/nativefiledialog dependencies/nfd
-pause
